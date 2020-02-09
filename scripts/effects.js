60a2da2c6847edbd804d67ea19ad2e6cf308de13
Willa Kong <willa.treee@gmail.com>
2020-02-09 00:48:48 -0500

    Filled more content

--- scripts/effects.js
+++ scripts/effects.js
@@ -1,84 +1,60 @@
 var W = document.body.offsetWidth;
 var H = document.body.offsetHeight;
-var scrollLink = $('.scroll');
 
-$( function() {
-  scrollLink.each(function(){
-    var sectionOffset = $(this.hash).offset().top - 40;
-    if (sectionOffset <= $(window).scrollTop()) {
-      scrollLink.removeClass('active');
-      $(this).addClass('active');
-    }
-  });
+var canvas = document.getElementById("effects");
+var ctx = canvas.getContext("2d");
 
-  scrollLink.click(function(e) {
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1000 );
-  });
+var max = 300;
+var particles = [];
 
+function getColor() {
+  return "rgba(" + Math.random()*255 + ", " + Math.random()*255 + ", "+ Math.random()*255 + ", 0.3)";
+};
 
-  var canvas = document.getElementById("effects");
-  var ctx = canvas.getContext("2d");
-
-  canvas.width = W;
-	canvas.height = H;
-
-  var max = 400;
-  var particles = [];
-
-	for (var i = 0; i < max; i++) {
+function initParticles() {
+  particles = [];
+  for (var i = 0; i < max; i++) {
 		particles.push({
 			x: Math.random()*W,
 			y: Math.random()*H,
 			r: Math.random()*5+1,
       d: Math.random()*max,
-      color: "rgba(" + Math.random()*255 + ", " + Math.random()*255 + ", "+ Math.random()*255 + ", 0.5)"
+      color: getColor()
 		})
   }
+};
 
-  function draw() {
-    ctx.clearRect(0, 0, W, H);
+function draw() {
+  ctx.clearRect(0, 0, W, H);
 
-    for (var i = 0; i < max; i++) {
-      var p = particles[i];
+  for (var i = 0; i < max; i++) {
+    var p = particles[i];
 
-      ctx.beginPath();
-      ctx.fillStyle = p.color;
-      ctx.moveTo(p.x, p.y);
-      ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
-      ctx.fill();
+    ctx.beginPath();
+    ctx.fillStyle = p.color;
+    ctx.moveTo(p.x, p.y);
+    ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
+    ctx.fill();
 
-      p.y += Math.cos(p.d) + 1 + (p.r / 2);
+    p.y += Math.cos(p.d) + 1 + (p.r / 2);
 
-      if(p.y > H) {
-        particles[i] = {
-          x: Math.random()*W,
-          y: -8,
-          r: p.r,
-          d: p.d,
-          color: p.color
-        }
+    if(p.y > H) {
+      particles[i] = {
+        x: Math.random()*W,
+        y: -8,
+        r: p.r,
+        d: p.d,
+        color: p.color
       }
     }
   }
+};
 
-	setInterval(draw, 40);
-});
-
-
-$( window ).scroll(function(){
-  var scrollPos = $(this).scrollTop();
-
-  scrollLink.each(function(){
-    var sectionOffset = $(this.hash).offset().top - 40;
-    if (sectionOffset <= scrollPos) {
-      scrollLink.removeClass('active');
-      $(this).addClass('active');
-    }
-  });
-  document.activeElement.blur()
+$( function() {
+  canvas.width = W;
+	canvas.height = H;
+	initParticles();
+  setInterval(draw, 40);
 });
 
 
@@ -88,6 +64,8 @@ $( window ).resize(function() {
 
   var canvas = document.getElementById("effects");
   canvas.width = W;
-	canvas.height = H;
+  canvas.height = H;
 
+  initParticles();
+  draw();
 });
