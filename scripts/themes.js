3555d8cc2df583b8f6252d203967b92f9c3a3080
Willa Kong <willa.treee@gmail.com>
2020-02-09 16:28:34 -0500

    theme

--- scripts/themes.js
+++ scripts/themes.js
@@ -3,43 +3,41 @@ let darkMode = "<i class=\"far fa-moon theme-icon\"></i>";
 
 let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
 let theme = sessionStorage.getItem('theme');
+let themeToggler = document.getElementById("theme-toggler");
 
-$( function() {
-  let themeToggler = document.getElementById("theme-toggler");
-  themeToggler.addEventListener ("click", changeTheme, false);
+themeToggler.addEventListener ("click", changeTheme, false);
 
+if (systemInitiatedDark.matches) {
+  themeToggler.innerHTML = lightMode;
+}
+else {
+  themeToggler.innerHTML = darkMode;
+}
+
+function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
+    document.documentElement.setAttribute('data-theme', 'dark');
     themeToggler.innerHTML = lightMode;
+     sessionStorage.setItem('theme', '');
   }
   else {
-    themeToggler.innerHTML = darkMode;
-  }
-
-  function prefersColorTest(systemInitiatedDark) {
-    if (systemInitiatedDark.matches) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      themeToggler.innerHTML = lightMode;
-       sessionStorage.setItem('theme', '');
-    }
-    else {
-      document.documentElement.setAttribute('data-theme', 'light');
-      themeToggler.innerHTML = darkMode;
-      sessionStorage.setItem('theme', '');
-    }
-  }
-  systemInitiatedDark.addListener(prefersColorTest);
-
-  if (theme === "dark") {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    sessionStorage.setItem('theme', 'dark');
-    themeToggler.innerHTML = lightMode;
-  }
-  else if (theme === "light") {
     document.documentElement.setAttribute('data-theme', 'light');
-    sessionStorage.setItem('theme', 'light');
     themeToggler.innerHTML = darkMode;
+    sessionStorage.setItem('theme', '');
   }
-});
+}
+systemInitiatedDark.addListener(prefersColorTest);
+
+if (theme === "dark") {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  sessionStorage.setItem('theme', 'dark');
+  themeToggler.innerHTML = lightMode;
+}
+else if (theme === "light") {
+  document.documentElement.setAttribute('data-theme', 'light');
+  sessionStorage.setItem('theme', 'light');
+  themeToggler.innerHTML = darkMode;
+}
 
 function changeTheme() {
   let theme = sessionStorage.getItem('theme');
