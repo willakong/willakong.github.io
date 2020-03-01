10bd02dbca80b4be1f30ea53958e83d5be846898
Willa Kong <willa.treee@gmail.com>
2020-03-01 13:24:35 -0500

    Scrolling fix

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,27 +1,41 @@
-var scrollLink = $('.scroll');
-
-function setActiveClass(scrollPos) {
-  scrollLink.each(function(){
-    var sectionOffset = $(this.hash).offset().top - 100;
-    if (sectionOffset <= scrollPos) {
-      scrollLink.removeClass('active');
-      $(this).addClass('active');
-    }
-  });
-}
-
 $( function() {
-  setActiveClass($(window).scrollTop());
+  var scrollLink = $('.scroll');
+  var lastId;
+  var topMenu = $("#topMenu");
+  var topMenuHeight = topMenu.outerHeight()+15;
 
-  scrollLink.click(function(e) {
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top - 60
-    }, 1000 );
+  scrollLink.click(function(e){
+    var href = $(this).attr('href'),
+        offsetTop = href === '#' ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+      }, 300);
+      e.preventDefault();
+    });
+
+
+  $( window ).scroll(function(){
+    var scrollPos = $(this).scrollTop() + topMenuHeight;
+
+    var curr = scrollLink.map(function(){
+      if ($(this).offset().top < scrollPos)
+        return this;
+    });
+    curr = curr[curr.length-1];
+    var id = curr && curr.length ? curr[0].id : "";
+
+    if (lastId !== id) {
+      scrollLink.filter("[href='#"+lastId+"']").removeClass("active");
+      lastId = id;
+      scrollLink.filter("[href='#"+id+"']").addClass("active");
+    }
+
+
   });
-});
 
-$( window ).scroll(function(){
-  var scrollPos = $(this).scrollTop();
-  setActiveClass(scrollPos);
+
+
+
+
+
 });
