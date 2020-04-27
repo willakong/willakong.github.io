a6fe6c8fa993e519db690fac657fcb6ab39258ff
Willa Kong <willa.treee@gmail.com>
2020-04-27 00:38:55 -0400

    Carousel arrows

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -3,6 +3,11 @@ $( function() {
     margin: 50,
     loop: true,
     dots: false,
+    nav: true,
+    navText : [
+      "<div class='nav-btn prev-slide'><i class='fa fa-chevron-left'></i></div>",
+      "<div class='nav-btn next-slide'><i class='fa fa-chevron-right'></i></div>"
+    ],
     responsive : {
       0 : {
         items: 1
